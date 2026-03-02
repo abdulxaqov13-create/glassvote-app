2874f1c55a08e34b232095c088e4840b0b88677e
Abdulxaqov <abdulxaqov13@gmail.com>
2026-03-02 17:31:19 +0500

    Fix: PORT turi Number-ga o'tkazildi

--- server.ts
+++ server.ts
@@ -601,10 +601,12 @@ async function startServer() {
     });
   }
 
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+ // Number() funksiyasi matnni songa aylantirib beradi
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 }
 
 startServer();
